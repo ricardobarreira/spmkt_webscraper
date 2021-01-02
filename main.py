2b83c4c36ffdcd92ab8bfef4efbd3df79f2896be
Ricardo Barreira <ricardobarreira.f@gmail.com>
2021-01-02 14:38:57 +0100

    Add comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from db_connection import create_session, get_headers, get_urls, get_store_html_elements, update_local_database
+import db_connection
 from scraper import get_html, scrape_info
 
 
-db_session = create_session()
+db_session = db_connection.create_session()
 
 grocery_stores = ['rewe', 'kaufland']
 
 for store in grocery_stores:
     # Get information needed to scrape store's website
-    headers = get_headers(db_session, store)
-    urls = get_urls(db_session, store)
-    html_elements = get_store_html_elements(db_session, store)
+    headers = db_connection.get_headers(db_session, store)
+    urls = db_connection.get_urls(db_session, store)
+    html_elements = db_connection.get_store_html_elements(db_session, store)
 
     offers_list = []
     for url in urls:
@@ -18,4 +18,4 @@ for store in grocery_stores:
         source = get_html(url, headers)
         # Parse the HTML data into a HTML Class from the requests_html module
         offers_list.extend(scrape_info(source, html_elements))
-    update_local_database(store, offers_list, db_session)
+    db_connection.update_local_database(store, offers_list, db_session)
