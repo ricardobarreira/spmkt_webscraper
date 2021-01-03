cb77ba5bcae58b3bdb3f1fe490efc6842678d21c
Ricardo Barreira <ricardobarreira.f@gmail.com>
2021-01-03 12:54:32 +0100

    Add remote postgresql db (heroku) session and add function call to update it everytime the pprogram is run

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import db_connection
 from scraper import get_html, scrape_info
 
 
-db_session = db_connection.create_session()
+local_db_session = db_connection.create_session()
+remote_db_session = db_connection.create_session(remote=True)
 
 grocery_stores = ['rewe', 'kaufland']
 
 for store in grocery_stores:
     # Get information needed to scrape store's website
-    headers = db_connection.get_headers(db_session, store)
-    urls = db_connection.get_urls(db_session, store)
-    html_elements = db_connection.get_store_html_elements(db_session, store)
+    headers = db_connection.get_headers(local_db_session, store)
+    urls = db_connection.get_urls(local_db_session, store)
+    html_elements = db_connection.get_store_html_elements(local_db_session, store)
 
     offers_list = []
     for url in urls:
@@ -18,4 +19,7 @@ for store in grocery_stores:
         source = get_html(url, headers)
         # Parse the HTML data into a HTML Class from the requests_html module
         offers_list.extend(scrape_info(source, html_elements))
-    db_connection.update_local_database(store, offers_list, db_session)
+
+    db_connection.update_local_database(store, offers_list, local_db_session)
+    #updated Heroku's Postgres database
+    db_connection.update_remote_database(store, offers_list, remote_db_session)
