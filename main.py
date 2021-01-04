c0751adec22758c4b5e83dd3f96df26117685f9b
Ricardo Barreira <ricardobarreira.f@gmail.com>
2021-01-04 10:50:27 +0100

    Change main for loop to iterate over index numbers instead of grocery store list elements. Fix bug of only storing the last grocery store offers on remote database by passing the index number as a parameter to update function

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ remote_db_session = db_connection.create_session(remote=True)
 
 grocery_stores = ['rewe', 'kaufland']
 
-for store in grocery_stores:
+for i in range(len(grocery_stores)):
     # Get information needed to scrape store's website
-    headers = db_connection.get_headers(local_db_session, store)
-    urls = db_connection.get_urls(local_db_session, store)
-    html_elements = db_connection.get_store_html_elements(local_db_session, store)
+    headers = db_connection.get_headers(local_db_session, grocery_stores[i])
+    urls = db_connection.get_urls(local_db_session, grocery_stores[i])
+    html_elements = db_connection.get_store_html_elements(local_db_session, grocery_stores[i])
 
     offers_list = []
     for url in urls:
@@ -20,6 +20,6 @@ for store in grocery_stores:
         # Parse the HTML data into a HTML Class from the requests_html module
         offers_list.extend(scrape_info(source, html_elements))
 
-    db_connection.update_local_database(store, offers_list, local_db_session)
+    db_connection.update_local_database(grocery_stores[i], offers_list, local_db_session)   
     #updated Heroku's Postgres database
-    db_connection.update_remote_database(store, offers_list, remote_db_session)
+    db_connection.update_remote_database(grocery_stores[i], offers_list, remote_db_session, i)
