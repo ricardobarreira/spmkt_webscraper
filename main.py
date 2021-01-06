e999d2f86182f08eb22878cf72dba70af2535ffb
Ricardo Barreira <ricardobarreira.f@gmail.com>
2021-01-06 13:13:29 +0100

    Catch exceptions from create_session() calls

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import db_connection
 from scraper import get_html, scrape_info
 import schedule
 import time
-from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
@@ -22,8 +21,20 @@ logger.addHandler(file_handler)
 
 
 def scrape_and_update():
-    local_db_session = db_connection.create_session()
-    remote_db_session = db_connection.create_session(remote=True)
+    try:
+        local_db_session = db_connection.create_session()
+        logger.info('Successfully created session for local database')
+    except:
+        logger.exception('Failed to create session for local database')
+        # If there's no access to the local database, the program can't access info to scrape the websites
+        return 0
+
+    try:
+        remote_db_session = db_connection.create_session(remote=True)
+        logger.info('Successfully created session for remote database')
+    except:
+        logger.exception('Failed to create session for remote database')
+        remote_db_session = False
 
     grocery_stores = ['rewe', 'kaufland']
 
@@ -46,14 +57,12 @@ def scrape_and_update():
             logger.exception(f'Failed to update local database for the store: {grocery_stores[i]}')
 
         # Update Heroku's Postgres database
-        try:
-            db_connection.update_remote_database(grocery_stores[i], offers_list, remote_db_session, i)
-            logger.info(f'Successfully updated remote database for store: {grocery_stores[i]}')
-        except:
-            logger.exception(f'Failed to update local database for the store: {grocery_stores[i]}')
-
-
-    print(f"\n*****Last Update: {datetime.now()}*****\n")
+        if remote_db_session:
+            try:
+                db_connection.update_remote_database(grocery_stores[i], offers_list, remote_db_session, i)
+                logger.info(f'Successfully updated remote database for store: {grocery_stores[i]}')
+            except:
+                logger.exception(f'Failed to update local database for the store: {grocery_stores[i]}')
 
 
 if __name__ == '__main__':
