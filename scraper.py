97fc89cd28ef231f976455163acfc08e24cd072c
Ricardo Barreira <ricardobarreira.f@gmail.com>
2021-01-28 14:02:11 +0100

    Refactor for better readability and add docstrings

--- scraper.py
+++ scraper.py
@@ -6,54 +6,65 @@ import re
 
 
 def get_html(url, headers):
-    r = urllib.request.Request(url=url, method='GET', headers=headers)
-    response = urllib.request.urlopen(r)
+    """Fetch data from the URL passed and return the HTML code
+
+    Keyword arguments:
+        url(string): a valid URL
+        headers(dict): strings for keys and values
+
+    Returns:
+        string:Full HTML code from webpage
+    """
+
+    r = urllib.request.Request(url=url, method='GET', headers=headers)    # Request object
+    response = urllib.request.urlopen(r)    # Bytes object with the data retrieved
     print(f'{url} RESPONSE: {response.status}')
     return response.read()
 
 
-def scrape_info(source, html_elements):
-    """Returns a list of retrieved offers, with each element being a dict"""
+def scrape_data(source, html_tag_obj_list):
+    """Take in HTML source code, find tags with data of interest, and return it in a list
 
-    # Parse the HTML data into a HTML Class from the requests_html module
-    data = [HTML(html=source)]
+    Keyword arguments:
+        source(string): HTML code
+        html_tag_obj_list(list): List elements are objects from the Html class in tables_objects module
 
-    # Divide html elements into two categories
-    high_level_divs = [i for i in html_elements if i.h_level > 0]
-    items_elements = [i for i in html_elements if i.h_level == 0]
+    Returns:
+        list:Each element is a dict containing offer attributes and their values
+    """
 
-    for div in high_level_divs:
-        # Crawl into the HTML structure until reach items of interest level
+    html_obj = [HTML(html=source)]     # Parse the HTML string into a HTML object from the requests_html module
+
+    high_lvl_html_divs = [i for i in html_tag_obj_list if i.h_level > 0]   # parent-tags for all product-tags
+    product_lvl_html_divs = [i for i in html_tag_obj_list if i.h_level == 0]
+
+    for div in high_lvl_html_divs:    # Crawl into the HTML structure until products lvl is reached
         try:
-            data = data[0].find(f'.{div.element_string}')
-        # TODO define which exception to catch
-        except:
+            html_obj = html_obj[0].find(f'.{div.class_attr}')
+
+        except:     # TODO define which exception to catch
             print("Could not access high level divs")
 
-    # Initialize list that will hold dicts containing the offers
     offers_list = []
-    for tag in data:
-        temp_dict = {}
+    for tag in html_obj:    # Iterates over all individual product tags
+        tmp_dict = {}
 
-        for element in items_elements:
+        for div in product_lvl_html_divs:
             try:
-                text_data = tag.find(f'.{element.element_string}')[0].text
+                text_data = tag.find(f'.{div.class_attr}')[0].text
 
-                # If the offer is from rewe, and the type is 'quantity', the retrieved text needs additional processing
-                if element.store_id == 1 and element.type == 'quantity':
-                    text_data = re.findall(r'je (.+)', text_data)[0]
+                if div.store_id == 1 and div.type == 'quantity':
+                    text_data = re.findall(r'je (.+)', text_data)[0]    # Clean quantity string for rewe offers
 
-                temp_dict[element.type] = text_data
-            # Failure to retrieve any value from this tag
-            # TODO specify exception
-            except:
-                temp_dict[element.type] = ""
+                tmp_dict[div.type] = text_data
+            except:     # TODO specify exception
+                tmp_dict[div.type] = ""
 
-        # Concatenates brand and title into titles for Kaufland offers
-        if 'brand' in temp_dict.keys():
-            temp_dict['title'] = f'{temp_dict["brand"]} {temp_dict["title"]}'.strip()
-            del temp_dict["brand"]
+        # Concatenates brand and title into one string for Kaufland offers
+        if 'brand' in tmp_dict.keys():
+            tmp_dict['title'] = f'{tmp_dict["brand"]} {tmp_dict["title"]}'.strip()
+            del tmp_dict["brand"]
 
-        offers_list.append(temp_dict)
+        offers_list.append(tmp_dict)
 
     return offers_list
